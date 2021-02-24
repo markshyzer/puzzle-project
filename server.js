2c823ca1b50ddfe203c04e6afde66a1964d4afc0
Mike Cao <mikecao369@gmail.com>
2021-02-24 17:11:23 -0500

    update

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ require('./config/database');
 
 app.use(logger('dev'));
 app.use(express.json());
-
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
